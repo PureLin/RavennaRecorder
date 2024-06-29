eaf0b29620606a7364b438d712dfe8888cab2c3d
PureLIN <Purelin@outlook.com>
2024-06-29 22:23:50 +0800

    test quickInstall.sh

---
+++ quickInstall.sh
@@ -0,0 +1,72 @@
+echo "Get dependencies libs and build tools\n"
+
+sudo apt-get update
+sudo apt install git cmake build-essential libsndfile1 libsndfile1-dev libboost-all-dev htop net-tools debhelper devscripts lockfile-progs -y
+
+echo "clone the project\n"
+
+git clone https://github.com/PureLin/RavennaRecorder.git RavennaRecorder
+cd RavennaRecorder
+
+echo "\nInstall usbmount\n"
+rm -rf usbmount
+set -e
+git clone https://github.com/rbrito/usbmount.git
+cd usbmount
+sudo dpkg-buildpackage -us -uc -b
+sudo dpkg -i ../usbmount_*.deb
+cd ..
+
+echo "\nBuild the project\n"
+
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j4
+
+echo "\nCopy build result\n"
+cp RavennaRecorder /usr/local/bin
+cd ..
+
+echo "\nCopy config page\n"
+cd resource
+if [ "$1" = "soundpuzzle" ]; then
+    echo "Use SoundPuzzle config page."
+    cd soundpuzzle
+fi
+cp ConfigMain.html $HOME/ConfigMain.html
+cd ..
+if [ "$1" = "soundpuzzle" ]; then
+    cd ..
+fi
+
+echo "\nCreate service\n"
+
+if [ -f "/etc/systemd/system/RavennaRecorder.service" ]; then
+    sudo rm /etc/systemd/system/RavennaRecorder.service
+fi
+echo "[Unit]" | sudo tee /etc/systemd/system/RavennaRecorder.service
+echo "Description=RavennaRecorder" | sudo tee -a /etc/systemd/system/RavennaRecorder.service
+echo "After=network.target" | sudo tee -a /etc/systemd/system/RavennaRecorder.service
+echo "" | sudo tee -a /etc/systemd/system/RavennaRecorder.service
+echo "[Service]" | sudo tee -a /etc/systemd/system/RavennaRecorder.service
+echo "Environment=\"HOME=$HOME\"" | sudo tee -a /etc/systemd/system/RavennaRecorder.service
+echo "ExecStartPre=/bin/sleep 3" | sudo tee -a /etc/systemd/system/RavennaRecorder.service
+echo "ExecStart=/usr/local/bin/RavennaRecorder" | sudo tee -a /etc/systemd/system/RavennaRecorder.service
+echo "Restart=always" | sudo tee -a /etc/systemd/system/RavennaRecorder.service
+echo "User=root" | sudo tee -a /etc/systemd/system/RavennaRecorder.service
+echo "Group=root" | sudo tee -a /etc/systemd/system/RavennaRecorder.service
+echo "" | sudo tee -a /etc/systemd/system/RavennaRecorder.service
+echo "[Install]" | sudo tee -a /etc/systemd/system/RavennaRecorder.service
+echo "WantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/RavennaRecorder.service
+
+echo "\nStart service\n"
+sudo systemctl daemon-reload
+sudo systemctl enable RavennaRecorder
+sudo systemctl start RavennaRecorder
+
+echo "\nInstallation complete, service status:\n"
+sudo systemctl status RavennaRecorder
+
+echo "\nYou can reboot the system to check if the service starts automatically."
