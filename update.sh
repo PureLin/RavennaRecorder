1746e861730d679ee4cdd7b77c858ac4c1fbf368
PureLIN <Purelin@outlook.com>
2024-08-30 22:22:11 +0800

    update config page

--- update.sh
+++ update.sh
@@ -1,4 +1,3 @@
-echo  -e "\n----Get latest version ----\n"
 rm /tmp/version.info
 curl -s https://api.github.com/repos/PureLin/RavennaRecorder/git/refs/heads/master > /tmp/version.info
 
@@ -14,13 +13,13 @@ fi
 cd ~
 
 echo -e "New version available, start update"
-echo -e "\n----clean up old repo----\n"
+echo -e "\n----clean up old repo----"
 rm -rf RavennaRecorder
 
-echo -e "\n----clone the project----\n"
+echo -e "\n----clone the project----"
 
 if [ -d "RavennaRecorder" ]; then
-    echo -e "\n----Old version of project found, remove it----\n"
+    echo -e "\n----Old version of project found, remove it----"
     rm -rf RavennaRecorder
 fi
 git clone https://github.com/PureLin/RavennaRecorder.git RavennaRecorder
@@ -29,7 +28,7 @@ cd RavennaRecorder
 sudo chmod +x dependency.sh
 sudo -E ./dependency.sh
 
-echo -e "\n----Build the project----\n"
+echo -e "\n----Build the project----"
 
 rm -rf build
 mkdir build
@@ -37,21 +36,21 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j4
 
-echo -e "\n----Copy build result----\n"
+echo -e "\n----Copy build result----"
 cp RavennaRecorder /usr/local/bin
 cd ..
 
-echo -e "\n----Copy config page----\n"
+echo -e "\n----Copy config page----"
 cd resource/soundpuzzle
 cp ConfigMain.html $HOME/ConfigMain.html
 cd ../..
 
-echo -e "\n----Remove Repo----\n"
+echo -e "\n----Remove Repo----"
 cd ~
 rm -rf RavennaRecorder
 
-echo -e "\n----Update version info----\n"
+echo -e "\n----Update version info----"
 curl https://api.github.com/repos/PureLin/RavennaRecorder/git/refs/heads/master > $HOME/version.info
 
-echo -e "\n----Update finished----\n"
+echo -e "\n----Update finished, Restart device to take effect----"
 exit 1
